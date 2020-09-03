b732890712d373f935df288aa2d06adc291ef5ff
Sam Passarelli <passarelli.sam@gmail.com>
2020-09-03 02:03:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,7 +15,11 @@ end
 
 introduction_with_language("Sam", "Ruby")
 
+def introduction_with_language_optional (name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
+introduction_with_language_optional("Sam")
 
 
 
